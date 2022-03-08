c89bbe0afb68818f00090161edbae3349f261605
Castelli Valerio <valeriocastellipro@gmail.com>
2022-03-08 11:25:35 +0100

    add front end

--- setup/header.php
+++ setup/header.php
@@ -34,11 +34,38 @@
 	<body>
 
 	<header>
-		<div class="d-flex justify-content-between bg-dark text-decoration-none text-white px-5">
+		<!-- <div class="d-flex justify-content-between bg-dark text-decoration-none text-white px-5">
 			<a class="my-auto text-uppercase" href="/index.html">Real ESTATE</a>
 
 			<a class="button my-auto p-3" href="/advert/add_advert.php">Ajout d'une annonce</a>
 
 			<a class="button my-auto p-3" href="/catalog.php">Afficher toutes les annonces</a>
+		</div> -->
+
+		<nav class="body">
+			<div class="w-50">
+				<a href="/index.php" id="logo" class="d-flex gap-2"><i class="bi bi-house-door-fill"></i>Metrium</a>
+			</div>
+
+			<div class="d-flex gap-4">
+				<a href="#">Support</a>
+				<a href="#" class="d-flex gap-2"><i class="bi bi-globe font-weight-normal"></i>Langage</a>
+				<a href="/advert/add_advert.php" id="btnAdd">Ajouter une annonce</a>
+			</div>
+
+			<div class="d-flex gap-2">
+				<a href="#"><i class="bi bi-bell"></i></a>
+				<a href="#"><i class="bi bi-person"></i></a>
+			</div>
+		</nav>
+
+		<div id="background">
+			<div id="panel">
+				<div class="cards">
+					<a class="btn btn-dark" href="/index.php">Afficher les 15 dernières annonces</a>
+
+					<a class="btn btn-dark" href="/catalog.php">Afficher toutes les annonces</a>
+				</div>
+			</div>				
 		</div>
 	</header>
